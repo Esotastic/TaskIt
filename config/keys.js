311a93218aa72806818e757b8433f8918a20fe66
adamszs <adamszs@email.sc.edu>
2019-01-22 21:29:06 -0500

    updated git ignore to remove cached version of keys.js

---
+++ config/keys.js
@@ -0,0 +1,21 @@
+//Go To MLabs.com and register to get a free AWS Database or upgrade your plan if needed
+//Once you register an account you can create a DB. Once created go to the Users tab and create a user and password
+//This will be used as your mongoURI to connect the api
+/**
+	Please use the default address and update with the appropriate username and password between the '<' and '>'
+
+	Example:
+	mongodb://<dbuser>:<dbpassword>@ds161794.mlab.com:61794/DBNAME
+
+	To connect using the mongo shell:
+	% mongo ds161794.mlab.com:61794/taskit -u <dbuser> -p <dbpassword>
+
+	
+**/
+
+
+
+
+module.exports = {
+    mongoURI: "mongodb://testUser:testPassword1@ds161794.mlab.com:61794/taskit"
+};
