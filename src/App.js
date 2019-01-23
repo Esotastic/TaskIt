5f6c83264ef16e56a21cbe7a87523ea8fd14962d
Christopher Martin <martincdev@gmail.com>
2019-01-22 20:40:01 -0600

    Added scss and basic login/register components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
