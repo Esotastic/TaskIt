12317e0bf97851d4626b13d77915d1449bf0db90
adamszs <zack.scott.adams@gmail.com>
2019-01-27 01:53:09 -0500

    added materialize-css framework, created homepage, added images and directories

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,5 +1,10 @@
 import React from "react";
+import './register.scss';
 const url = "/";
+
+
+
+
 export default class Register extends React.Component {
   state = {
     fullName: "",
@@ -32,39 +37,38 @@ export default class Register extends React.Component {
 
   render() {
     return (
-      <form>
-        
-        <input
-          name="fullName"
-          placeholder="Full Name"
-          value={this.state.fullName}
-          onChange={e => this.change(e)}
-        />
-        <br />
-        <input
-          name="userName"
-          placeholder="Username"
-          value={this.state.userName}
-          onChange={e => this.change(e)}
-        />
-        <br />
-        <input
-          name="email"
-          placeholder="Email"
-          value={this.state.email}
-          onChange={e => this.change(e)}
-        />
-        <br />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={this.state.password}
-          onChange={e => this.change(e)}
-        />
-        <br />
-        <button onClick={e => this.onSubmit(e)}>Submit</button>
+      <div class="container">
+      <form id="register" class="col s12 blue-grey lighten-5 z-depth-3 center-align text-center-align">
+        <div class="row">
+          <div class="input-field col s6 offset-s3">
+            <i class="material-icons prefix">account_circle</i>
+            <input className="fullName" id="icon_prefix" type="text" />
+            <label for="icon_prefix">Full Name</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s6 offset-s3">
+            <i class="material-icons prefix">email</i>
+            <input id="icon_prefix" type="email" />
+            <label for="icon_prefix">Email</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s6 offset-s3">
+            <i class="material-icons prefix">contacts</i>
+            <input id="icon_prefix" type="text" />
+            <label for="icon_prefix">Username</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s6 offset-s3">
+            <i class="material-icons prefix">security</i>
+            <input id="icon_prefix" type="password"/>
+            <label for="icon_prefix">Password</label>
+          </div>
+        </div>
       </form>
+    </div>
     );
   }
 };
