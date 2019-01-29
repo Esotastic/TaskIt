c385e40c0ac4f444a30ebb8333eff7fa238a1a7f
EllioSmoov <efwschroeder@gmail.com>
2019-01-29 14:35:42 -0500

    Pre css modules commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React, { Component } from "react";
 import Register from "./components/register/Register";
-<<<<<<< HEAD
 import Header from "./components/Header/Header";
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-=======
->>>>>>> 8f468742d6ee20548ec836d46a44ac0d261bd058
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      // <div id="banner-one">
-      //   <div className="App">
-      //     <div className="container"><Register /></div>
-      //   </div>
-      // </div>
-      <div>
-        <BrowserRouter>
-          <div className="container">
-            <Header />
-          </div>
-        </BrowserRouter>
+      <div id="banner-one">
+        <div className="App">
+          <div className="container"><Header /></div>
+          <div className="container"><Register /></div>
+        </div>
       </div>
+      // <div>
+      //   <BrowserRouter>
+      //     <div className="container">
+      //       <Header />
+      //     </div>
+      //   </BrowserRouter>
+      // </div>
     );
   }
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,38 +1,53 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+
+import styles from './Header.scss';
 
 class Header extends Component {
-    renderContent() {
-        switch (this.props.auth) {
-            case null:
-                return;
-            case false:
-                return [
-                    <li><a href="/login">Login</a></li>,
-                    <li><a href="/register">Register</a></li>
-                ];
-            default:
-                return [
-                    <li><a href="/dashboard">Dashboard</a></li>,
-                    <li><a href="/logout">Logout</a></li>
-                ];
-        }
-    }
+    //     renderContent() {
+    //         switch (this.props.auth) {
+    //             case null:
+    //                 return;
+    //             case false:
+    //                 return [
+    //                     <li><a href="/login">Login</a></li>,
+    //                     <li><a href="/register">Register</a></li>
+    //                 ];
+    //             default:
+    //                 return [
+    //                     <li><a href="/dashboard">Dashboard</a></li>,
+    //                     <li><a href="/logout">Logout</a></li>
+    //                 ];
+    //         }
+    //     }
 
     render() {
+
         return (
-            <nav>
-                <Link
-                    to="/"
-                    className="brand-logo"
-                >
-                    TaskIt
-                </Link>
-                <ul>
-                    {this.renderContent()}
-                </ul>
-            </nav>
-        )
+            <div>
+                <nav className={styles.Nav}>
+                    <div className="nav-wrapper">
+                        <a href="#" class="brand-logo">TaskIt</a>
+                        <ul id="nav-mobile" class="right">
+                            <li ><a href="login.html">Login</a></li>
+                            <li><a href="#">Link 02</a></li>
+                            <li><a href="#">Link 03</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            //             <nav>
+            //                 <Link
+            //                     to="/"
+            //                     className="brand-logo"
+            //                 >
+            //                     TaskIt
+            //                 </Link>
+            //                 <ul>
+            //                     {this.renderContent()}
+            //                 </ul>
+            //             </nav>
+        );
     }
 }
 
