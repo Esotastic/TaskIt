6b0baaf5290f88ea42b0072394b8b3ac2de41828
EllioSmoov <efwschroeder@gmail.com>
2019-01-29 17:54:14 -0500

    Prepare for for git push

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,53 +1,22 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
 
-// import styles from './Header.scss';
 import './Header.scss';
 
 class Header extends Component {
-    //     renderContent() {
-    //         switch (this.props.auth) {
-    //             case null:
-    //                 return;
-    //             case false:
-    //                 return [
-    //                     <li><a href="/login">Login</a></li>,
-    //                     <li><a href="/register">Register</a></li>
-    //                 ];
-    //             default:
-    //                 return [
-    //                     <li><a href="/dashboard">Dashboard</a></li>,
-    //                     <li><a href="/logout">Logout</a></li>
-    //                 ];
-    //         }
-    //     }
-
     render() {
 
         return (
             <div>
                 <nav className="Nav">
                     <div className="nav-wrapper">
-                        <a href="#" class="brand-logo">TaskIt</a>
-                        <ul id="nav-mobile" class="right">
-                            <li ><a href="login.html">Login</a></li>
-                            <li><a href="#">Link 02</a></li>
-                            <li><a href="#">Link 03</a></li>
+                        <a href="/" className="brand-logo">TaskIt</a>
+                        <ul id="nav-mobile" className="right">
+                            <li ><a href="/login">Login</a></li>
+                            <li><a href="/register">Register</a></li>
                         </ul>
                     </div>
                 </nav>
             </div >
-            //             <nav>
-            //                 <Link
-            //                     to="/"
-            //                     className="brand-logo"
-            //                 >
-            //                     TaskIt
-            //                 </Link>
-            //                 <ul>
-            //                     {this.renderContent()}
-            //                 </ul>
-            //             </nav>
         );
     }
 }
